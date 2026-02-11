3eeaa37d1c72617287fef0d7333f94176a04d57b
Ivan Konstantinov <ivan.konstantinov@ontotext.com>
2026-02-11 15:46:13 +0200

    WIP : wizard

---
+++ app/src/main/java/com/vanxacloud/appstudio/proximity/wizard/Wizard.java
@@ -0,0 +1,70 @@
+package com.vanxacloud.appstudio.proximity.wizard;
+
+import com.vanxacloud.appstudio.proximity.ProximityApp;
+import javafx.collections.ObservableMap;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ButtonType;
+import org.controlsfx.dialog.WizardPane;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Wizard {
+
+
+    private final int numPages;
+    private org.controlsfx.dialog.Wizard wizard;
+
+    public Wizard() {
+        this(1);
+    }
+
+    public Wizard(int numPages) {
+        this.numPages = numPages;
+        try {
+            initializeWizard();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private void initializeWizard() throws IOException {
+        List<WizardPane> pages = new ArrayList<>();
+        for (int page = 1; page <= numPages; page++) {
+            WizardPane pane = new WizardPane();
+            pane.getStylesheets().add("/style.css");
+            pane.getStyleClass().remove("wizard-pane"); // Remove annoying png in default wizard-pane
+            pane.setContent(FXMLLoader.load(ProximityApp.class.getResource(String.format("wizard/wizard-page-%d.fxml", page))));
+            pages.add(pane);
+        }
+        // Show wizard and handle result
+        this.wizard = new org.controlsfx.dialog.Wizard();
+        this.wizard.getDialog().setResizable(true);
+        wizard.setTitle("Wizard");
+        wizard.setFlow(new org.controlsfx.dialog.Wizard.LinearFlow(pages));
+    }
+
+    public Settings show() {
+        Optional<ButtonType> buttonTypeOptional = wizard.showAndWait();
+        if (buttonTypeOptional.isEmpty()) {
+            System.out.println("Wizard not completed");
+            return null;
+        }
+        ButtonType buttonType = buttonTypeOptional.get();
+        if (buttonType.equals(ButtonType.CANCEL)) {
+            System.out.println("Wizard cancelled");
+        } else if (buttonType.equals(ButtonType.OK)) {
+            System.out.println("Wizard completed");
+        } else if (buttonType.equals(ButtonType.FINISH)) {
+            return new Settings(wizard.getSettings());
+        }
+        return null;
+    }
+
+    public record Settings(ObservableMap<String, Object> settings) {
+
+    }
+}
